24a73c01508e66efd5400dcdc0ad138a697371b6
Andy <andrewcuccinello@gmail.com>
2021-08-18 13:01:04 -0400

    Remove unused imports, pull listeners up

--- src/system/actor/sheet/DGActorSheet.ts
+++ src/system/actor/sheet/DGActorSheet.ts
@@ -15,17 +15,20 @@
  */
 
 import { DGPercentileRoll } from '../../dice/DGPercentileRoll';
-import { DGDamageRoll } from '../../dice/DGDamageRoll';
+import { DamagePartType, DGDamageRoll, DGDamageRollPart } from '../../dice/DGDamageRoll';
 import { CSS_CLASSES, SYSTEM_NAME } from '../../Constants';
 import { ChatMessageDataConstructorData } from '@league-of-foundry-developers/foundry-vtt-types/src/foundry/common/data/data.mjs/chatMessageData';
 import { PercentileModifierDialog, PercentileModifierDialogResults } from '../../dialog/PercentileModifierDialog';
 import { DamageModifierDialog, DamageModifierDialogResults } from '../../dialog/DamageModifierDialog';
-import { preprocessEvent } from '../../util/Sheet';
+import { preprocessEvent, preprocessEventWithId } from '../../util/Sheet';
 import { DGActor } from '../DGActor';
 import { DGSkill } from '../../item/DGSkill';
 import { DGWeapon } from '../../item/DGWeapon';
 import { DGArmor } from '../../item/DGArmor';
 import { DGGear } from '../../item/DGGear';
+import { DGItem } from '../../item/DGItem';
+import { DEFAULT_ITEM_NAME, ItemTypeBond, ItemTypeDisorder, ItemTypeMotivation, ItemTypeWeapon, StatisticType } from '../../../types/Constants';
+import { ItemType } from '../../../types/Item';
 
 export interface SkillGroup {
     name: string;
@@ -192,9 +195,254 @@ export abstract class DGActorSheet<TOptions extends DGActorSheetOptions, TData e
         return renderData;
     }
 
+    /**
+     * Bind hooks for items without a specific type, e.g. universally bound hooks.
+     * @param html JQuery wrapper for the html to bind over.
+     * @private
+     */
+    protected bindUniversalItemListeners(html: JQuery) {
+        // Items: Cross an item
+        html.find('label.cross-item').on('click', async (event) => {
+            const { id } = preprocessEventWithId(event);
+            const item = this.actor.getEmbeddedDocument('Item', id) as DGItem;
+            if (item.data.type === ItemTypeBond || item.data.type === ItemTypeMotivation || item.data.type === ItemTypeDisorder) {
+                await item.update({
+                    ['data.crossed.value']: !item.data.data.crossed.value,
+                });
+            }
+        });
+        // Items: Add a new item
+        html.find('label.add-item').on('click', async (event) => {
+            const target = preprocessEvent(event);
+            const type = target.data('type') as ItemType;
+            await this.actor.createEmbeddedDocuments('Item', [
+                {
+                    type: type,
+                    name: DEFAULT_ITEM_NAME[type],
+                },
+            ]);
+        });
+        // Items: Edit an item
+        html.find('label.edit-item').on('click', async (event) => {
+            const { id } = preprocessEventWithId(event);
+            const item = this.actor.getEmbeddedDocument('Item', id) as DGItem;
+            if (item && item.sheet) {
+                item.sheet.render(true);
+            }
+        });
+        // Items: Delete an item
+        html.find('label.delete-item').on('click', async (event) => {
+            const { id } = preprocessEventWithId(event);
+            await this.actor.deleteEmbeddedDocuments('Item', [id]);
+        });
+
+        // Items: Inline update of check boxes
+        html.find('input.modify-item[type="checkbox"]').on('change', async (event) => {
+            const { target, id } = preprocessEventWithId(event);
+            const value = target.prop('checked') as boolean;
+            const path = target.data('path') as string;
+
+            await this.actor.updateEmbeddedDocuments('Item', [
+                {
+                    _id: id,
+                    [path]: value,
+                },
+            ]);
+        });
+        // Items: Inline update of inputs
+        html.find('input.modify-item:not([type="checkbox"])').on('change', async (event) => {
+            const { target, id } = preprocessEventWithId(event);
+            let value = target.val() as string | number;
+            const path = target.data('path') as string;
+
+            if (target.attr('type') === 'number') {
+                value = parseInt(value.toString());
+            }
+
+            await this.actor.updateEmbeddedDocuments('Item', [
+                {
+                    _id: id,
+                    [path]: value,
+                },
+            ]);
+        });
+    }
+
+    /**
+     * Bind misc roll listeners.
+     * @param html JQuery wrapper for the html to bind over.
+     * @private
+     */
+    protected bindRollListeners(html: JQuery) {
+        // Skill: Roll skill
+        html.find('div.list-item.skill label.name').on('click', async (event) => {
+            const { id } = preprocessEventWithId(event);
+
+            let roll: DGPercentileRoll;
+            if (event.ctrlKey) {
+                const skillName = this.actor.getSkillName(id);
+                const dialogResults = await this.promptPercentileModifier(skillName!);
+                roll = await this.actor.rollSkill(id, [
+                    {
+                        label: game.i18n.localize('DG.DICE.rollModifier'),
+                        value: dialogResults.modifier,
+                    },
+                ]);
+            } else {
+                roll = await this.actor.rollSkill(id);
+            }
+
+            await this.sendRollToChat(roll);
+        });
+
+        // Luck: Roll luck
+        html.find('section.attributes label.clickable.luck').on('click', async (event) => {
+            preprocessEvent(event);
+
+            let roll: DGPercentileRoll;
+            if (event.ctrlKey) {
+                const dialogResults = await this.promptPercentileModifier(`DG.DICE.luckCheck`);
+                roll = await this.actor.rollLuck([
+                    {
+                        label: game.i18n.localize('DG.DICE.rollModifier'),
+                        value: dialogResults.modifier,
+                    },
+                ]);
+            } else {
+                roll = await this.actor.rollLuck();
+            }
+
+            await this.sendRollToChat(roll);
+        });
+
+        // Stats: Roll stats*5
+        html.find('div.stats-field label.clickable.stats').on('click', async (event) => {
+            const { id } = preprocessEventWithId(event);
+
+            let roll: DGPercentileRoll;
+            if (event.ctrlKey) {
+                const dialogResults = await this.promptPercentileModifier('DG.DICE.statisticCheck');
+                roll = await this.actor.rollStatistic(id as StatisticType, [
+                    {
+                        label: game.i18n.localize('DG.DICE.rollModifier'),
+                        value: dialogResults.modifier,
+                    },
+                ]);
+            } else {
+                roll = await this.actor.rollStatistic(id as StatisticType);
+            }
+
+            await this.sendRollToChat(roll);
+        });
+    }
+
+    /**
+     * Bind inventory related listeners.
+     * @param html JQuery wrapper for the html to bind over.
+     * @private
+     */
+    protected bindInventoryListeners(html: JQuery) {
+        // Inventory: Roll attack
+        html.find('div.inventory-group.weapon label.attack').on('click', async (event) => {
+            const { id } = preprocessEventWithId(event);
+            const item = this.actor.getEmbeddedDocument('Item', id) as DGWeapon;
+            let roll: DGPercentileRoll;
+            if (event.ctrlKey) {
+                const skillName = this.actor.getSkillName(item.data.data.skill.value);
+                const dialogResults = await this.promptPercentileModifier(skillName!);
+                roll = await this.actor.rollSkill(item.data.data.skill.value, [
+                    {
+                        label: game.i18n.localize('DG.DICE.rollModifier'),
+                        value: dialogResults.modifier,
+                    },
+                ]);
+            } else {
+                roll = await this.actor.rollSkill(item.data.data.skill.value);
+            }
+            await this.sendRollToChat(roll);
+        });
+
+        // Inventory: Roll damage
+        html.find('div.inventory-group.weapon label.damage').on('click', async (event) => {
+            const { id } = preprocessEventWithId(event);
+            const item = this.actor.getEmbeddedDocument('Item', id) as DGWeapon;
+
+            let lethalityRoll: DGPercentileRoll | undefined = undefined;
+            let damageRoll: DGDamageRoll;
+
+            if (event.ctrlKey) {
+                if (item.data.data.lethality.value > 0) {
+                    const percentileDialogResults = await this.promptPercentileModifier('DG.DICE.lethalityCheck');
+                    lethalityRoll = await this.actor.rollLethalityForWeapon(id, [
+                        {
+                            label: game.i18n.localize('DG.DICE.rollModifier'),
+                            value: percentileDialogResults.modifier,
+                        },
+                    ]);
+                }
+
+                const damageDialogResults = await this.promptDamageModifier('DG.DICE.damageRoll');
+
+                let modifiers: DGDamageRollPart[] = [];
+                if (damageDialogResults.add) {
+                    modifiers.push({
+                        value: damageDialogResults.add,
+                        label: game.i18n.localize('DG.DICE.rollModifier'),
+                        type: DamagePartType.Add,
+                    });
+                }
+                if (damageDialogResults.multiply) {
+                    modifiers.push({
+                        value: damageDialogResults.multiply,
+                        label: game.i18n.localize('DG.DICE.rollModifier'),
+                        type: DamagePartType.Multiply,
+                    });
+                }
+
+                damageRoll = await this.actor.rollDamageForWeapon(id, modifiers);
+            } else {
+                if (item.data.data.lethality.value > 0) {
+                    lethalityRoll = await this.actor.rollLethalityForWeapon(id);
+                }
+                damageRoll = await this.actor.rollDamageForWeapon(id);
+            }
+
+            if (lethalityRoll) {
+                await this.sendRollToChat(lethalityRoll, false);
+            }
+            await this.sendRollToChat(damageRoll);
+        });
+
+        // Inventory: Decrement ammo
+        html.find('div.inventory-group.weapon div.ammo label.decrement').on('click', async (event) => {
+            const { id } = preprocessEventWithId(event);
+            const item: DGItem = this.actor.getEmbeddedDocument('Item', id) as DGItem;
+            if (item.data.type === ItemTypeWeapon) {
+                await item.update({
+                    ['data.ammo.value']: item.data.data.ammo.value - 1,
+                });
+            }
+        });
+
+        // Inventory: Reload ammo
+        html.find('div.inventory-group.weapon div.ammo label.reload').on('click', async (event) => {
+            const { id } = preprocessEventWithId(event);
+            const item: DGItem = this.actor.getEmbeddedDocument('Item', id) as DGItem;
+            if (item.data.type === ItemTypeWeapon) {
+                await item.update({
+                    ['data.ammo.value']: item.data.data.ammo.max,
+                });
+            }
+        });
+    }
+
     public activateListeners(html: JQuery) {
         super.activateListeners(html);
 
         this.bindCollapsibleListeners(html);
+
+        this.bindInventoryListeners(html);
+        this.bindRollListeners(html);
+        this.bindUniversalItemListeners(html);
     }
 }
